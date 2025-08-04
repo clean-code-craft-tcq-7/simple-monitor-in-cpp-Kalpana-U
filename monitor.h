cde3db6eeae014c9dbded280fece0474d102aeea
Kalpana-U <kalpanathangarani578@gmail.com>
2025-08-04 19:53:21 +0530

    Included string header files

--- monitor.h
+++ monitor.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 int vitalsOk(float temperature, float pulseRate, float spo2);
 void displayVitalAlert(const std::string& message);
 int isTemperatureOk(float temperature);
